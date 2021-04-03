6bb93221d74ab58786347d13560905b9434e828e
U201817044 <1219821955@qq.com>
2021-04-03 13:31:56 +0800

    更新了游戏设置文件

--- settings.py
+++ settings.py
@@ -3,8 +3,9 @@ class Settings:
     def __init__(self):
         """初始化游戏设置"""
         # 设定屏幕的基本参数
-        self.screen_width = 1200
-        self.screen_height = 700
+        self.screen_width = 960
+        self.screen_height = 540
         self.bg_color = (230, 230, 230)
-        self.screen_size = (1200, 700)
+        self.screen_size = (self.screen_width, self.screen_height)
+        self.game_name = "Alien Invasion"
 
