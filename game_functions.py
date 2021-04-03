ef9416615d7113dedb54f4e91258d4cbea7d025e
U201817044 <1219821955@qq.com>
2021-04-03 13:32:44 +0800

    游戏功能部分，新增了事件响应函数以及屏幕更新函数

---
+++ game_functions.py
@@ -0,0 +1,46 @@
+import pygame
+import sys
+
+
+def check_events(ship):
+    """监测鼠标和键盘响应事件"""
+
+    for event in pygame.event.get():
+        # 点击关闭按钮
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        # 飞船的操作部分
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                ship.right = True
+            elif event.key == pygame.K_a:
+                ship.left = True
+            elif event.key == pygame.K_w:
+                ship.up = True
+            elif event.key == pygame.K_s:
+                ship.down = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                ship.right = False
+            elif event.key == pygame.K_a:
+                ship.left = False
+            elif event.key == pygame.K_w:
+                ship.up = False
+            elif event.key == pygame.K_s:
+                ship.down = False
+
+
+def update_screen(screen, screen_settings, ship):
+    """
+    更新屏幕
+
+    :param screen: 需要更新的屏幕
+    :param screen_settings: 屏幕设置参数
+    :param ship: 需要在屏幕上绘制的飞船
+    :return:
+    """
+    screen.fill(screen_settings.bg_color)
+    ship.blit_me()
+    pygame.display.flip()
